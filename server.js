65fe9e701729a7d9ffce6238c4207b6730275152
tomhco <me@tomharry.com>
2014-11-24 23:58:38 +0000

    Added Server Code

--- server.js
+++ server.js
@@ -1,4 +1,50 @@
 /* jslint node: true */
 'use strict';
 
-console.log('Hello World');
+var mqtt = require('mqtt');
+var mongodb = require('mongodb');
+var mongodbClient = mongodb.MongoClient;
+var mongodbURI = 'mongodb://localhost:27017/mqttmap';
+var deviceRoot = 'demo/device/';
+var collection, client;
+
+mongodbClient.connect(mongodbURI, setupCollection);
+
+function setupCollection(err, db) {
+
+	if (err) throw err;
+	collection = db.collection("test_mqtt");
+	client = mqtt.createClient(1883, 'localhost');
+	client.subscribe(deviceRoot + "+");
+	client.on('message', insertEvent);
+
+}
+
+function insertEvent(topic, payload) {
+
+	var key = topic.replace(deviceRoot, '');
+
+	collection.update({
+			_id: key
+		}, {
+
+			$push: {
+				events: {
+					event: {
+						value: payload,
+						when: new Date()
+					}
+				}
+			}
+
+		}, {
+			upsert: true
+		},
+
+		function (err, docs) {
+			if (err) {
+				console.log("Insert fail");
+			}
+		}
+	)
+}
